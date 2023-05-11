8011b7cfe378239dec9cca072f2f9f994b9b296b
WillC919 <william.p.lee@stonybrook.edu>
2023-05-11 17:51:09 -0400

    init

--- client/src/components/fsoMain.js
+++ client/src/components/fsoMain.js
@@ -2,7 +2,7 @@ import Left from './fsoLeft.js'
 import Right from './fsoRight.js'
 import '../stylesheets/fsoMain.css'
 
-export default function Main({pageIndex, setPageIndex, questsData, setQuestsData, tagsData, setTagsData}) {
+export default function Main({pageIndex, setPageIndex, questsData, setQuestsData, questIndex, setQuestIndex, tagsData, setTagsData}) {
     return (
         <div id="main">
             <Left  pageIndex = {pageIndex} setPageIndex = {setPageIndex} setQuestsData = {setQuestsData}/>
